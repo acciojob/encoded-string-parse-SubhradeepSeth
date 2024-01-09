be787855f72ce10e92ac1dd71a68b27224e0db8c
SubhradeepSeth <139810175+SubhradeepSeth@users.noreply.github.com>
2024-01-09 21:40:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const parseCode = (str) => {
   // your code here
-};
+	let parts=str.split('0');
+	const arr=parts.filter((e)=> e!=="");
 
+	return {
+		firstName:arr[0],
+		lastName:arr[1],
+		id:arr[2]
+	}
+};
 // Do not change the code below
 const str = prompt("Enter str: ");
 alert(JSON.stringify(parseCode(str)));
